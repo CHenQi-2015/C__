6f484414b2e9bc35830fc07bd67f7bb21c8b2e01
CHenQi2015 <chenqi2015_@outlook.com>
2026-06-13 20:49:42 +0800

    a

--- Temp/main.cpp
+++ Temp/main.cpp
@@ -8,7 +8,12 @@ struct Node
 {
     int x;
     int y;
-}tag[N];
+}p[N];
+bool cmp(Node a,Node b)
+{
+    if (a.x!=b.x)return a.x<b.x;
+    return a.y<b.y;
+}
 signed main(){
     cin>>n;
     for (int i=1;i<=n;i++)
