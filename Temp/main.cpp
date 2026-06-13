bbe691635de2751e0de3dcc0e24ac9859674cf66
CHenQi2015 <chenqi2015_@outlook.com>
2026-06-13 20:19:21 +0800

    commit

--- Temp/main.cpp
+++ Temp/main.cpp
@@ -4,16 +4,6 @@ using namespace std;
 priority_queue<int>s,b;
 int n,idx;
 signed main(){
-    cin>>n;
-    int ans;
-    int m;
-    cin>>m;
-    ans=m;
-    for (int i=2;i<=n;i++)
-    {
-        int m;
-        cin>>m;
-        
-    }
+
     return 0;
 }
