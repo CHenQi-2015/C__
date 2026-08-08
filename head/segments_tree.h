ca24ede25709c22d9402f1c0c3f111a1bb414f0c
CHenQi2015 <chenqi2015_@outlook.com>
2026-08-08 11:20:19 +0800

    add head--segments_tree.h

--- head/segments_tree.h
+++ head/segments_tree.h
@@ -1,6 +1,5 @@
-#ifndef seg_t
-#define seg_t
-#define ll long long
+#ifndef SEGMENT_TREE_H
+#define SEGMENT_TREE_H
 #include<vector>
 class SMT
 {
@@ -9,27 +8,43 @@ class SMT
  *    tr[]  线段树
  *    tag[] 懒标签
 */
+//#define ll long long
+//typedef long long ll;
+using ll=long long;//=#define ll long long 或 typedef long long ll;
 public:
-    SMT(ll m):n(m),a(m),tr(m<<2),tag(m<<2){}
+    SMT(ll m,const std::vector<ll>&arr):n(m),a(m+5),tr((m<<2)+5),tag((m<<2)+5)
+    {
+        for (ll i=1;i<=m;i++)
+        {
+            a[i]=arr[i];//1-index编码
+        }
+        return;
+    }
+    void build(){build(1,1,n);}
+    void update(ll qlt,ll qrt,ll v){update(1,1,n,qlt,qrt,v);}
+    ll query(ll qlt,ll qrt){return query(1,1,n,qlt,qrt);}
+    void point_update(ll qpt,ll v){update(1,1,n,qpt,qpt,v);}
+    ll point_query(ll qpt){return query(1,1,n,qpt,qpt);}
 private:
     ll n;
     std::vector<ll>a,tr,tag;
-    inline ll ls(ll r)
+    static constexpr ll ls(ll r)
     {
         return r<<1;
     }
-    inline ll rs(ll r)
+    static constexpr ll rs(ll r)
     {
         return r<<1|1;
     }
-    inline void push_up(ll r)
+    void push_up(ll r)
     {
         tr[r]=tr[ls(r)]+tr[rs(r)];//操作(+,-,^,max,min等)
         return;
     }
-    inline void push_down(ll r,ll lt,ll rt)
+    void push_down(ll r,ll lt,ll rt)
     {
         //向下传递懒标签，并更新数据
+        if (!tag[r])return;
         ll mid=(lt+rt)>>1;
         tr[ls(r)]=tr[ls(r)]+(mid-lt+1)*tag[r];//操作(+,-,^,max,min等)
         tag[ls(r)]=tag[ls(r)]+tag[r];//操作(+,-,^,max,min等)
@@ -40,7 +55,7 @@ private:
         return;
     }
     //构建线段树
-    inline void build(ll r,ll lt,ll rt){
+    void build(ll r,ll lt,ll rt){
         //对 [lt,rt] 区间构建线段树,当前根的编号为 r
         if(lt==rt){
             tr[r]=a[lt];
@@ -53,7 +68,7 @@ private:
         return;
     }
     //区间修改
-    inline void update(ll r,ll lt,ll rt,ll qlt,ll qrt,ll v){
+    void update(ll r,ll lt,ll rt,ll qlt,ll qrt,ll v){
         if(qlt<=lt&&qrt>=rt){
             //在整个范围内
             tr[r]=tr[r]+(rt-lt+1)*v;//操作(+,-,^,max,min等)
@@ -68,41 +83,13 @@ private:
         return;
     }
     //区间查询
-    inline ll query(ll r,ll lt,ll rt,ll qlt,ll qrt){
+    ll query(ll r,ll lt,ll rt,ll qlt,ll qrt){
         if(qrt<lt||qlt>rt)return 0;//不在范围内
         if(qlt<=lt&&qrt>=rt)return tr[r];//在整个范围内
         push_down(r,lt,rt);
         ll mid=((lt+rt)>>1);
-        return query(r<<1,lt,mid,qlt,qrt)/*左半部分*/+query(r<<1,mid+1,rt,qlt,qrt)/*右半部分*/;
+        return query(ls(r),lt,mid,qlt,qrt)/*左半部分*/+query(rs(r),mid+1,rt,qlt,qrt)/*右半部分*/;
     }
-    //单点修改
-    inline void point_update(ll r,ll lt,ll rt,ll qpt,ll v){
-        if(lt>qpt||rt<qpt){
-            //不在范围内
-            return;
-        }
-        if(lt==rt){
-            //单点
-            tr[r]=tr[r]+v;//操作(+,-,^,max,min等)
-            return;
-        }
-        ll mid=((lt+rt)>>1);
-        point_update(ls(r),lt,mid,qpt,v);//左半部分
-        point_update(rs(r),mid+1,rt,qpt,v);//右半部分
-        push_up(r);//向上更新
-        return;
-    }
-    //单点查询
-    inline ll point_query(ll r,ll lt,ll rt,ll qpt){
-        if(lt>qpt||rt<qpt){
-            return 0;//不在范围内
-        }
-        if(lt==rt){
-            return tr[r];//单点
-        }
-        ll mid=(lt+rt)>>1;
-        return point_query(ls(r),lt,mid,qpt)/*左半部分*/+point_query(rs(r),mid+1,rt,qpt)/*右半部分*/;
-    }
+//#undef ll
 };
-#undef ll
 #endif
